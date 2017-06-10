1a5924d6552f1dd3adf99af127d83e7f8876f4d3
ThauNguyen <hoangthau627@gmail.com>
2017-06-10 10:28:46 +0700

    add home page, sign up, login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutes } from './app.routing';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignUpComponent } from './sign-up/sign-up.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomePageComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
